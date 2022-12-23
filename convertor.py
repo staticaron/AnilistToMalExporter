30e75d6cad219f4fff62493f55880a80d2ae26f1
VikaDev <devm19102003@gmail.com>
2022-12-23 21:23:26 +0530

    FIX : Start Date being the Airing Date ADD : Rating Transfer ADD : Finish Date Transfer

--- convertor.py
+++ convertor.py
@@ -21,17 +21,23 @@ query($username:String){
       entries{
         progress
         status
+        score
+        startedAt {
+            year
+            month
+            day
+        }
+        completedAt{
+            year
+            month
+            day
+        }
         media{
           id
           idMal
           title{
             english
             romaji
-          }
-          startDate {
-            year
-            month
-            day
           }
         }
       }
@@ -69,46 +75,33 @@ def convert(anilist_username:str) -> None:
         """Populating Anime"""
         for anime_info in list_entries:
 
-            anilist_id = anime_info.get("media").get("id")
             mal_id = anime_info.get("media").get("idMal")
             anime_title = anime_info.get("media").get("title").get("english") or anime_info.get("media").get("title").get("romaji")
-            anime_startDate = "{y}-{m}-{d}".format(y=anime_info.get("media").get("startDate").get("year"), m=anime_info.get("media").get("startDate").get("month"), d=anime_info.get("media").get("startDate").get("day"))
-            anime_progress  = anime_info.get("progress")
             anime_status = status_mappings.get(anime_info.get("status"))
+            anime_score = anime_info.get("score")
+            anime_progress  = anime_info.get("progress")
+            anime_startDate = "{y}-{m}-{d}".format(y=anime_info.get("startedAt").get("year"), m=anime_info.get("startedAt").get("month"), d=anime_info.get("startedAt").get("day"))
+            anime_endDate = "{y}-{m}-{d}".format(y=anime_info.get("completedAt").get("year"), m=anime_info.get("completedAt").get("month"), d=anime_info.get("completedAt").get("day"))
 
             if anime_title == "None" or anime_title is None:
                 anime_info.get("media").get("title").get("english")
                 anime_info.get("media").get("title").get("romaji")
 
-            try:
-                anime_sources = requests.get(url=FINDMYLIST_BASE.format(id=mal_id)).json()[0].get("sources")
-            except:
-                print(">>>>>>>>")
-                print("ERROR : \nTITLE : {}\nANILIST ID : {}".format(anime_title, anime_id))
-                print(">>>>>>>>")
-                continue
-
-            anime_id = 0
-
-            for source in anime_sources:
-                source = str(source)
-
-                if "anidb" in source:
-                    anime_id = source.split("/")[-1]
-
             anime_container = ET.SubElement(root, "anime")
 
-            ET.SubElement(anime_container, "series_animedb_id").text = str(anime_id)
-            ET.SubElement(anime_container, "anilist_id").text = str(anilist_id)
-            ET.SubElement(anime_container, "title").text = anime_title
+            ET.SubElement(anime_container, "series_animedb_id").text = str(mal_id)
             ET.SubElement(anime_container, "my_watched_episodes").text = str(anime_progress)
-            ET.SubElement(anime_container, "my_start_date").text = anime_startDate
+            ET.SubElement(anime_container, "my_score").text = str(anime_score)
+            if anime_status != "Plan to Watch":
+                ET.SubElement(anime_container, "my_start_date").text = anime_startDate
+            if anime_status == "Completed":
+                ET.SubElement(anime_container, "my_finish_date").text = anime_endDate
             ET.SubElement(anime_container, "my_status").text = anime_status
             ET.SubElement(anime_container, "update_on_import").text = "1"
 
             print("ADDED : {} > {} ".format(anime_status, anime_title))
 
-    with open("{}_MAL.xml".format(anilist_username), "wb") as xml_in:
+    with open("{}_MALids.xml".format(anilist_username), "wb") as xml_in:
         
         tree = ET.ElementTree(root)
         tree.write(xml_in)
